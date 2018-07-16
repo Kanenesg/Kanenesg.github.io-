06f5a286997966012dd57a4dad5654c9d2e6cc35
Kanenesg <41303155+Kanenesg@users.noreply.github.com>
2018-07-16 15:16:10 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// alert("hello world");
+
+function bobi(){
+  document.getElementById("title").style.fontSize = "200px";
+}
+function bobo() {
+  document.getElementById("title").style.fontSize = "50px";
+}
+
+function changesong() {
+  document.getElementById('mysong').src = "song2.mp3";
+}
